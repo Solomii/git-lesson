078d6eda730499beef884840a6ee51dad61432ca
Solomii <kushniryksolomiya@gmail.com>
2022-04-01 18:36:03 +0300

    [+] did task1 dotCalculator
    [+] did task2 'grid of the alphabet'

--- task1/index.js
+++ task1/index.js
@@ -1,4 +1,30 @@
-function calculate() {
-  //Here is your solution
-  return
+function dotCalculator(equation) {
+  let spl = equation.split(" ");
+  let firstPartNumberOfDots = spl[0].length;
+  let operation = spl[1];
+  let secondPartNumberOfDots = spl[2].length;
+  let numberOfDotsInResult;
+  
+    if (operation === '+') {
+    numberOfDotsInResult = firstPartNumberOfDots + secondPartNumberOfDots;
+  } else if (operation === '-') {
+     numberOfDotsInResult = firstPartNumberOfDots - secondPartNumberOfDots;
+  } else if (operation ==='*') {
+     numberOfDotsInResult = firstPartNumberOfDots * secondPartNumberOfDots;
+  } else if (operation ==='//') {
+    numberOfDotsInResult = Math.floor(firstPartNumberOfDots / secondPartNumberOfDots);
+  } else {
+    throw Error("Wrong input!")
+  }
+  return '.'.repeat(numberOfDotsInResult)
 }
+
+console.log(dotCalculator("..... + ..............."));
+console.log(dotCalculator("..... - ..."));
+console.log(dotCalculator("..... - ."));
+console.log(dotCalculator("..... * ..."));
+console.log(dotCalculator("..... * .."));
+console.log(dotCalculator("..... // .."));
+console.log(dotCalculator("..... // ."));
+console.log(dotCalculator(". // .."));
+console.log(dotCalculator(".. - .."));
--- task2/index.js
+++ task2/index.js
@@ -1,4 +1,32 @@
-function calculate() {
-  //Here is your solution
-  return
+function grid(N) {
+  if (N < 0) {
+    return null;
+  };
+
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let gritAlphabet = "";
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      let index = j + i;
+      if (index >= 26) {
+        index %= 26;
+      }
+      gritAlphabet += alphabet[index];
+        if (j < N - 1) {
+        gritAlphabet += " ";
+      }
+    }
+    if (i < N - 1) {
+      gritAlphabet += "\n" 
+    }
+ }
+  
+  console.log(gritAlphabet);
+  return gritAlphabet;
 }
+
+grid(4);
+grid(10);
+// grid(25);
+// grid(26);
+// grid(90);
