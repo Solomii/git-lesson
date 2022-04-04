8ad56337fa118effcdb136e1a9adc9812029d586
Solomii <kushniryksolomiya@gmail.com>
2022-04-04 14:27:24 +0300

    [#2] Add task №7 from JSExercise

--- taskJS/index.js
+++ taskJS/index.js
@@ -52,3 +52,18 @@ function map(fn, array) {
  }
   return newArr;
 }
+
+// 7. Напиши функцию bind, которая позволяет привязать
+// контекст(значение this) к функции:
+
+// window.x = 1;
+// var ctx = { x: 2 };
+
+// function testThis(a) { console.log("x=" + this.x + ", a=" + a); }
+// console.log(testThis(100)); // x=1, a=100
+// var boundFunction = bind(testThis, ctx);
+// console.log(boundFunction(100)); // x=2, a= 100;
+  
+function bind(fn, context) {
+  return fn.bind(context);
+}
