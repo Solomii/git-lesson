ab3ddab40ede8c03226744b7de190b0121d7d944
Solomii <kushniryksolomiya@gmail.com>
2022-04-04 14:22:54 +0300

    [#2] Add  task  №2 from JSExercise

--- taskJS/index.js
+++ taskJS/index.js
@@ -15,3 +15,18 @@ function sequence(start = 0, step = 1) {
     return returnValue;
  }
 }
+
+// 2.Также, нужна функция take(fn, count)
+// которая вызвает функцию fn заданное число(count)
+// раз и возвращает массив с результатами вызовов.
+// Она нам пригодится для отладки:
+// var gen2 = sequence(0, 2);
+// console.log(take(gen2, 5)); // [0, 2, 4, 6, 8 ]
+
+function take(fn, count) {
+  let arr = [];
+  for (i=0; i<count; i++) {
+  arr[i] = fn();
+ }
+ return arr;
+}
